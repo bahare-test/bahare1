baaba171b97919225075a1eb193a08ea90e28533
root <mhmdmghaderi@gmail.com>
2023-01-17 10:39:49 +0330

    pull request commit2

--- azi.php
+++ azi.php
@@ -2,6 +2,7 @@
 	//test comment
 	// salam
 	// pull request comment
+	//pull requseet comment2
     ini_set('display_errors', '1');
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
